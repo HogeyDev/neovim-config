13a2cdf2ebbee6b4a011dfcfe96d31579263e956
Kourtet <evanhoge123@gmail.com>
2023-12-01 15:54:19 -0500

    Working LSP!!! Basic lsp bindings added too.

--- lua/kourtet/lsp.lua
+++ lua/kourtet/lsp.lua
@@ -1,6 +1,37 @@
+local lsp_servers = { "lua_ls", "tsserver", "rust_analyzer" }
+
 require('mason').setup()
 require('mason-lspconfig').setup({
-	ensure_installed = { "lua_ls", "tsserver" }
+	ensure_installed = lsp_servers
 })
 
-require('lsp-zero').setup_servers({ 'lua_ls', 'tsserver' })
+local lsp = require('lsp-zero')
+local cmp = require('cmp')
+local cmp_action = lsp.cmp_action()
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+lsp.setup_servers(lsp_servers)
+
+cmp.setup({
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<CR>'] = cmp.mapping.confirm({select = true}),
+
+		['<C-Space>'] = cmp_action.toggle_completion(),
+		['<Esc>'] = cmp.mapping.close(),
+
+		['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+		['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
+
+		-- Scroll up and down the documentation window
+		['<C-u>'] = cmp.mapping.scroll_docs(-4),
+		['<C-d>'] = cmp.mapping.scroll_docs(4),
+
+		-- Navigate between snippet placeholders
+		['<C-f>'] = cmp_action.luasnip_jump_forward(),
+		['<C-b>'] = cmp_action.luasnip_jump_backward(),
+	}),
+})
